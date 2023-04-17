a3f973897ee38fdc2ea8cb3815fd68fb42aa7947
Sheetal-V <109294489+Sheetal-V@users.noreply.github.com>
2023-04-17 12:24:17 +0530

    Changes has been updated

---
+++ scriptclock.js
@@ -0,0 +1,23 @@
+const second = document.querySelector('.clk-hour');
+  const min = document.querySelector('.clk-hour');
+  const hourHand = document.querySelector('.clk-hour');
+
+  function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    second.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    min.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  }
+
+  setInterval(setDate, 1000);
+
+  setDate();
